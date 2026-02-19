596cfe989c6e141f1c603aa3f778ad7f1b85e2c9
sinchan27 <gsinchan27@gmail.com>
2026-02-19 18:06:48 +0530

    add root route

--- backend/routes/algo_routes.js
+++ backend/routes/algo_routes.js
@@ -2,6 +2,9 @@ const express = require("express");
 const { bubble_sorting, merge_sorting } = require("../controller/algo_con.js");
 const router = express.Router();
 
+router.get("/test", (req, res) => {
+  res.json({ message: "API working" });
+});
 
 router.post("/bubble_sort", bubble_sorting);
 router.post("/merge_sort",merge_sorting);
